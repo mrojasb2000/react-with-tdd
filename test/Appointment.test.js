14522a420b3800128119837b0beef786c0b65867
Mauricio Rojas <mrojasb2000@gmail.com>
2025-12-01 23:49:44 -0300

    chore: add basic unit test component react

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,5 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { act } from "react";
+import { Appointment } from "../src/Appointment";
 
 describe("Appointment", () => {
+  let container;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.replaceChildren(container);
+  });
+
+  const render = (component) =>
+    act(() => ReactDOM.createRoot(container).render(component));
+
   it("renders the customer first name", () => {
+    const customer = { firstName: "Ashley" };
+    render(<Appointment customer={customer} />);
+
+    expect(document.body.textContent).toContain("Ashley");
   });
 });
