a3dd89113b3567d93672b0e4782e16500b53f95f
Mauricio Rojas <mrojasb2000@gmail.com>
2025-12-05 23:03:15 -0300

    chore: selects the first appointment by default

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -35,8 +35,8 @@ describe("AppointmentsDayView", () => {
   const today = new Date();
 
   const twoAppointments = [
-    { startAt: today.setHours(12, 0) },
-    { startAt: today.setHours(13, 0) },
+    { startAt: today.setHours(12, 0), customer: { firstName: "Ashley" } },
+    { startAt: today.setHours(13, 0), customer: { firstName: "Jordan" } },
   ];
 
   beforeEach(() => {
@@ -81,5 +81,10 @@ describe("AppointmentsDayView", () => {
     expect(document.body.textContent).toContain(
       "There are no appointments scheduled for today.",
     );
+  });
+
+  it("selects the first appointment by default", () => {
+    render(<AppointmentsDayView appointments={twoAppointments} />);
+    expect(document.body.textContent).toContain("Ashley");
   });
 });
