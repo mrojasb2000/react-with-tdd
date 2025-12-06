12533e04873185f991fe27859310c87ed38b14f3
Mauricio Rojas <mrojasb2000@gmail.com>
2025-12-06 14:23:07 -0300

    chore: renders another appointment when selected

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const appointmentTimeOfDay = (startsAt) => {
   // destructuring the hours and minutes from the arrayOk
@@ -8,21 +8,24 @@ const appointmentTimeOfDay = (startsAt) => {
 
 export const Appointment = ({ customer }) => <div>{customer.firstName}</div>;
 
-export const AppointmentsDayView = ({ appointments }) => (
-  <div id="appointmentsDayView">
-    <ol>
-      {appointments.map((appointment) => (
-        <li key={appointment.startAt}>
-          <button type="button">
-            {appointmentTimeOfDay(appointment.startAt)}
-          </button>
-        </li>
-      ))}
-    </ol>
-    {appointments.length === 0 ? (
-      <p>There are no appointments scheduled for today.</p>
-    ) : (
-      <Appointment {...appointments[0]} />
-    )}
-  </div>
-);
+export const AppointmentsDayView = ({ appointments }) => {
+  const [selectedAppointment, setSelectedAppointment] = useState(0);
+  return (
+    <div id="appointmentsDayView">
+      <ol>
+        {appointments.map((appointment, i) => (
+          <li key={appointment.startAt}>
+            <button type="button" onClick={() => setSelectedAppointment(i)}>
+              {appointmentTimeOfDay(appointment.startAt)}
+            </button>
+          </li>
+        ))}
+      </ol>
+      {appointments.length === 0 ? (
+        <p>There are no appointments scheduled for today.</p>
+      ) : (
+        <Appointment {...appointments[selectedAppointment]} />
+      )}
+    </div>
+  );
+};
